f7a902493e552d8b09108df228ca18556bc1f7dc
Nicolas Haenni <mail@nicolashaenni.me>
2019-05-11 10:35:42 +0200

    Enhance Shot Object

--- gameObjects/Shot.cpp
+++ gameObjects/Shot.cpp
@@ -1,9 +1,9 @@
 #include <QString>
 #include "Shot.h"
 
-Shot::Shot(int x, int y) : GameObject(x, y, QString("img/banana"), 40)
+Shot::Shot(int x, int y, int speed, int angle) : GameObject(x, y, QString("img/banana"), 40), speed(speed), angle(angle), t(0)
 {
-    
+
 }
 
 void Shot::move()
